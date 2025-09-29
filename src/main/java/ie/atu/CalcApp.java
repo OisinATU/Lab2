2a0d30a7af28c86b71ad8edd23eacb93b8a60819
OisinLoftus-STUDENT <oisinloftus@hotmail.com>
2025-09-29 10:16:57 +0100

    Created add operation

--- src/main/java/ie/atu/CalcApp.java
+++ src/main/java/ie/atu/CalcApp.java
@@ -1,4 +1,27 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class CalcApp {
+    public static void main(String[] args) {
+        //Setup scanner
+        Scanner scan1 = new Scanner(System.in);
+
+        //get operation
+        System.out.print("Please enter an operation: ");
+        String operation = scan1.nextLine();
+        System.out.println(operation);
+
+        Calculator myCalc = new Calculator();
+
+        int return_value = myCalc.add_numbers(2,4);
+        System.out.println(return_value);
+
+
+        //get first value
+
+        //get second value
+
+
+    }
 }
